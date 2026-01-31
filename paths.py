23a3ec055199c8343c12378bfb9ef830b03b1d8b
Moritz <moritz.eigenauer@gmail.com>
2026-01-31 17:39:31 +0100

    make paths relative

--- paths.py
+++ paths.py
@@ -1,1 +1,16 @@
-TRANSLATIONS_FILE = "translations.toml"
+from pathlib import Path
+
+def get_project_root() -> Path:
+    """Returns the root directory of the project."""
+    current_path = Path(__file__).resolve()
+
+    for parent in current_path.parents:
+        if (parent / ".git").exists():
+            return parent
+        if (parent / "requirements.txt").exists():
+            return parent
+
+    return current_path.parent.parent
+
+ROOT_DIR = get_project_root()
+TRANSLATIONS_FILE = ROOT_DIR / "translations.toml"
